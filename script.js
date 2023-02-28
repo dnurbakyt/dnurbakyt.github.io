82fd629916e93318ba5c7c9abb8cc21c2173ce7b
dnurbakyt <123709966+dnurbakyt@users.noreply.github.com>
2023-02-28 15:07:47 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-let Teams = [
-{
-  name: 'Dallas Mavericks',
-  favPlayer: 'Luka Doncic',
-  titles: 0,
-  
-},
-{
-  name: 'Los Angeles Lakers',
-  favPlayer: 'LeBron James',
-  titles: 4,
-}, 
-{
-  name: 'Chicago Bulls',
-  favPlayer: 'Michel Jordan',
-  titles: 6,
-}
+const cars = [ 
+    {
+        "company": "Hennessey",
+        "model": "Venom",
+        "max-speed": 485
+    },
+    {
+        "company": "Bugatti",
+        "model": "Chiron Super Sport",
+        "max-speed": 490
+    },
+    {
+        "company": "Koenigsegg",
+        "model": "Jesko",
+        "max-speed": 483
+    },
+    {
+        "company": "9ff",
+        "model": "GT9-R",
+        "max-speed": 413
+    }
 ]
 
-
-for (let i = 0; i < Teams.length; i++) {
-  document.write("<h5>Team: " + Teams[i]["name"] + "</h5>");
-  document.write("<h5>Favourite player: " + Teams[i]["favPlayer"] + "</h5>");
-  document.write("<h5>Titles: " + Teams[i]["titles"] + "</h5>");
-  document.writeln("")
+function speed_sum(cars) {
+    sum = 0;
+    for(let i = 0; i < 4; i++){
+        sum += cars[i]['max-speed'];
+    }
+    return sum;
 }
 
-function re(Teams){
-  return re;
+function first_car(cars){
+    return cars[0]
 }
-console.log(Teams)
+
+document.write("<h3>Car company: " + first_car(cars)["company"] + "</h3>")
+document.write("<h3>Model: " + first_car(cars)["model"] + "</h3>")
+document.write("<h3>Max speed: " + first_car(cars)["max-speed"] + "</h3>")
+document.write("\n\n")
+
+for(let i = 1; i < 4; i++){
+    document.write("<h3>Car company: " + cars[i]["company"] + "</h3>")
+    document.write("<h3>Model: " + cars[i]["model"] + "</h3>")
+    document.write("<h3>Max speed: " + cars[i]["max-speed"] + "</h3>")
+    document.write("\n\n")
+}
+
+document.write("<h3>Average speed: " + speed_sum(cars) / 4 + " km/h</h3>")
+
